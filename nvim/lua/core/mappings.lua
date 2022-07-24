ecfbdea58181242961c9fda6f0e44eaf3e6fa945
pat_sky <patricio.gutierrez@cohu.com>
2022-07-24 16:23:49 -0700

    - maps to nav in insert move, collude with luasnip maps

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -13,14 +13,6 @@ local M = {}
 
 M.general = {
 
-  i = {
-    -- navigate within insert mode
-    ['<C-h>'] = { '<Left>',   '  move left' },
-    ['<C-l>'] = { '<Right>',  '  move right' },
-    ['<C-j>'] = { '<Down>',   '  move down' },
-    ['<C-k>'] = { '<Up>',     '  move up' },
-  },
-
   n = {
     -- general
     ['<ESC>']     = { ':noh<CR>', '  no highlight' },
