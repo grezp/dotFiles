f9d7b463d531640f2b7e917246980e2594ca6bed
pat_sky <patricio.gutierrez@cohu.com>
2022-09-09 15:24:01 -0400

    tokyonight colors broke darken/lighten
    number line: fix colors
    vars: changed color var names for better understanding

--- nvim/lua/custom/colors.lua
+++ nvim/lua/custom/colors.lua
@@ -3,7 +3,6 @@ local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
 if not status_ok then
   vim.notify('colorscheme ' .. colorscheme .. ' not found!')
-  vim.cmd('colorscheme desert')
   return
 end
 
@@ -13,22 +12,24 @@ local config = require('tokyonight.config')
 local util   = require('tokyonight.util')
 local c = colors.setup(config)
 
+-- bg colors
 local dark3   = c.dark3
-local blue55  = util.darken(c.blue, 0.55)
+local blue_d55  = util.darken(c.blue, 0.55)
 local status  = c.bg_statusline
 local status2 = util.darken(c.dark3, 0.2)
 
-local purple = c.purple
-local green  = c.green2
-local blue   = c.blue
-local red    = c.red
-local orange = c.orange
-local green2 = c.green1
--- local violet1 = util.darken( c.magenta, 0.7)
--- local violet2 = util.darken( c.magenta, 0.5)
--- local magenta = c.magenta
--- local bg_hi  = c.bg_highlight
+-- fg colors
+local purple  = c.purple
+local green   = c.green2
+local blue    = c.blue
+local red     = c.red
+local orange  = c.orange
+local green2  = c.green1
+local blue7_d85   = util.lighten(c.blue7, 0.85)
+local blue_d7     = util.darken(c.blue, 0.6)
+local comment_l85 = util.lighten(c.comment, 0.9)
 
+-- cusor colors
 local cursor_bg = '#bb9af7'
 local cursor_fg = '#1f2335'
 
@@ -41,13 +42,13 @@ end
 
 M.custom_hl_groups = {
   -- status line
-  StatuslineNC    = { fg = dark3,   bg = status },
-  AltToBg         = { fg = status2, bg = status },
-  AltToCh         = { fg = status2, bg = green2 },
-  StatusLineAlt   = { fg = blue55,  bg = status2 },
-  StatusLineNCAlt = { fg = status2, bg = status },
-  FileMods        = { fg = status,  bg = green2, bold = true },
-  FileModsInv     = { fg = green2,  bg = status },
+  StatuslineNC    = { fg = dark3,     bg = status },
+  AltToBg         = { fg = status2,   bg = status },
+  AltToCh         = { fg = status2,   bg = green2 },
+  StatusLineAlt   = { fg = blue_d55,  bg = status2 },
+  StatusLineNCAlt = { fg = status2,   bg = status },
+  FileMods        = { fg = status,    bg = green2, bold = true },
+  FileModsInv     = { fg = green2,    bg = status },
 
   -- mode colors
   NormalModeStatus    = { fg = status, bg = purple },
@@ -68,20 +69,16 @@ M.custom_hl_groups = {
   Cursor  = { fg = cursor_fg, bg = cursor_bg },
   lCursor = { fg = cursor_fg, bg = cursor_bg },
 
+  -- number line
+  LineNr        = { fg = blue7_d85 },
+  CursorLineNr  = { fg = blue_d7 },
+
+  Comment  = { fg = comment_l85, italic = true },
+
   -- plugins --
 
   -- indent_blankline
   IndentBlanklineContextChar = { fg = c.blue },
-
-  -- lightspeed
-  -- LightspeedOneCharMatch    = { fg = bg_hi,   bg = violet2 },
-  -- LightspeedPendingOpArea   = { fg = bg_hi,   bg = violet2 },
-  -- LightspeedShortcut        = { fg = bg_hi,   bg = violet2 },
-  -- LightspeedLabel           = { fg = violet1, bg = bg_hi,   bold = true, underline = true },
-  -- LightspeedLabelOverlapped = { fg = violet1, bg = bg_hi,   bold = true, underline = true },
-
-  -- HopNextKey  = { fg = bg_hi, bg = magenta },
-  -- HopNextKey  = { fg = bg_hi, bg = violet2 },
 }
 
 M.apply_highlight(M.custom_hl_groups)
